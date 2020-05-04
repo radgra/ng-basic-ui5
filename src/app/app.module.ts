d46c3b69992213b1d7ef6404721aaaa643fb7c71
Radek <radgra2@gmail.com>
2020-05-04 17:31:06 +0200

    added ui5  webcomponents module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
+import { Ui5WebComponentsModule } from './modules/ui5-web-components/ui5-web-components.module';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MasterPageComponent } from './master-page/master-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MasterPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Ui5WebComponentsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
---
+++ src/app/modules/ui5-web-components/ui5-web-components.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import "@ui5/webcomponents/dist/Button";
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+})
+export class Ui5WebComponentsModule { }
