75aaed4aeecb774f0750921cc940a45e76cb5a72
Radek <radgra2@gmail.com>
2020-05-04 19:53:51 +0200

    corrected tabs

--- src/app/resume-page/resume-page.component.ts
+++ src/app/resume-page/resume-page.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resume-page.component.scss']
 })
 export class ResumePageComponent implements OnInit {
-
+  private TABS = {
+    0:'Home',
+    1:'New',
+    2:'We',
+    3:'About',
+  }
+  selectedTab:string = 'New'
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onTabSelected(event:CustomEvent) {
+    const index = event.detail.tabIndex
+    this.selectedTab = this.TABS[index]
   }
 
 }
